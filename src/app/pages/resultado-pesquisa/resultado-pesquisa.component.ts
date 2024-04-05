c4e0c4a53f00d21579d5571a7ef36ad522bdfbf6
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2024-04-05 01:18:38 -0300

    feat: Adicionando a página de 404 not-found

--- src/app/pages/resultado-pesquisa/resultado-pesquisa.component.ts
+++ src/app/pages/resultado-pesquisa/resultado-pesquisa.component.ts
@@ -28,5 +28,17 @@ export class ResultadoPesquisaComponent {
         error => console.error('Error fetching movie details:', error)
       );
   }
+
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+  
+    const formattedDay = day < 10 ? '0' + day : day;
+    const formattedMonth = month < 10 ? '0' + month : month;
+  
+    return `${formattedDay}-${formattedMonth}-${year}`;
+  }
   
 }
