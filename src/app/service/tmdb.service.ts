4453577d87c3ac1570c4c05735b3408e99b9a568
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2024-04-10 12:37:20 -0300

    refactor: Retirado o apiKey do service para que o mesmo seja colocado no environment.ts

--- src/app/service/tmdb.service.ts
+++ src/app/service/tmdb.service.ts
@@ -1,17 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map, catchError } from 'rxjs';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TmdbService {
-  
+
   private baseUrl = 'https://api.themoviedb.org/3';
-  private apiKey = 'e29649a70cf989a53de3b7b7d45aa2c8';
+  private apiKey = environment.apiKey;
   private language = 'pt-BR';
 
-  constructor(private http: HttpClient) { } 
+  constructor(private http: HttpClient) { }
 
   searchMovies(query: string, page: number = 1): Observable<any> { // Adicionado parâmetro 'page' com valor padrão 1
     const url = `${this.baseUrl}/search/movie?api_key=${this.apiKey}&query=${query}&page=${page}`; // Adicionado '&page=${page}' à URL
@@ -38,9 +41,9 @@ export class TmdbService {
 
   getPopularMovies(page: number, itemsPerPage: number): Observable<any> {
     const url = `${this.baseUrl}/movie/popular?api_key=${this.apiKey}&language=${this.language}&page=${page}`;
-  
+
     return this.http.get<any>(url).pipe(
-      map(response => response.results.slice(0, itemsPerPage)), 
+      map(response => response.results.slice(0, itemsPerPage)),
       catchError(error => {
         console.error('Error:', error);
         throw error;
