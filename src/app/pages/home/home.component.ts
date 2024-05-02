64f46c26bad5d6b13ff414e6a88686d6bea9968c
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2024-05-02 16:22:54 -0300

    refactor: Adicionado efeitos no nome do site e ajustes na conta

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,7 +13,7 @@ export class HomeComponent implements OnInit {
   searchResults: any[] = [];
   searchQuery: string = '';
   currentPage: number = 1;
-  itemsPerPage: number = 18;
+  itemsPerPage: number = 20;
   searchClicked = false;
   searchPage: number = 1;
 
